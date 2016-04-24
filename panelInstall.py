96897b33f4573d891253c70cc81738e7fc45c5fb
Anthony Smith <asmith@cari.net>
2016-04-24 11:49:53 -0700

    Almost done with this.

---
+++ panelInstall.py
@@ -0,0 +1,153 @@
+import os
+import socket
+import fcntl
+import struct
+import tarfile
+from getpass import getpass
+
+# cPanel
+
+def cPanel():
+  url = 'https://securedownloads.cpanel.net/latest'
+  path = '/home'
+  os.system("yum install -y perl gcc")
+  os.chdir(path)
+  os.system("curl -o latest -L %s && sh latest" % (url))
+
+# Plesk
+
+def plesk():
+  url = 'http://autoinstall.plesk.com/plesk-installer'
+  release = 'plesk'
+  build = ('cd /home && wget %s && sh plesk-installer --select-product-id %s --select-release-latest --installation-type Full --notify-email service@cari.net' % (url, release))
+  os.system(build)
+
+# LAMP Stack
+
+def LAMP():
+  root_pass = getpass("What pass would you like to use for mysql root account?: ")
+  stack = (apache, mysql, php)
+  for block in stack:
+    if block is mysql:
+      block(root_pass)
+    else:
+      block()
+
+def apache():
+  services = ('httpd',)
+  yum_engine(services)
+
+def mysql(root_pass):
+  services = ('mysql-server',)
+  yum_engine(services)
+  mysql_secure(root_pass)
+
+def php():
+  services = ('php', 'php-mysql',)
+  yum_engine(services)
+
+def yum_engine(services):
+  for s in services:
+    os.system('yum install -y %s' % (s))
+
+# Wordpress
+
+def wordpress():
+  url = 'http://wordpress.org/latest.tar.gz'
+  wp_pass = getpass("Please choose a password for the Wordpress MySQL user: ")
+  #LAMP()
+  get_wordpress(url)
+  set_database(wp_pass)
+  set_config(wp_pass)
+  #os.system("cp -r /home/wordpress/* /var/www/html")
+  #yum_engine(('php-gd',))
+  #os.system("service httpd restart")
+  
+def get_wordpress(url):
+  os.system("cd /home && wget %s" % (url))
+  tar = tarfile.open('/home/latest.tar.gz')
+  tar.extractall()
+
+def set_database(wp_pass):
+  comm_list = ("CREATE DATABASE wordpress",
+		"CREATE USER 'wp_user'@'localhost'",
+		"SET PASSWORD FOR 'wp_user'@'localhost' = PASSWORD('%s')" % (wp_pass),
+		"GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost' IDENTIFIED BY '%s'" % (wp_pass),
+		"FLUSH PRIVILEGES")
+  mysql_bash_engine(comm_list, auth=True, wp_pass=wp_pass)
+
+def set_config(wp_pass):
+  path = '/home/wordpress'
+  settings = {'database_name_here': 'wordpress', 
+		'username_here': 'wp_user',
+		'password_here': wp_pass}
+  os.system("cp %s/wp-config-sample.php %s/wp-config.php" % (path, path))
+  replace_engine(path + '/wp-config.php', settings)
+
+def replace_engine(path, r_dict):
+  data = None
+  with open(path, 'r') as f:
+    data = f.read()
+  for key, value in r_dict.items():
+    data = data.replace(key, value)
+  with open(path, 'w') as f:
+    f.write(data)
+
+# MySQL Secure Install
+
+def mysql_secure(root_pass):
+  hname = get_info()[1]
+  secure_list = ("UPDATE mysql.user SET Password = PASSWORD('%s') WHERE User = 'root'" % (root_pass),
+		 "DROP USER ''@'localhost'",
+		 "DROP USER ''@'%s'" % (hname),
+		 "DROP DATABASE test",
+		 "FLUSH PRIVILEGES")
+  mysql_bash_engine(secure_list)
+
+def mysql_bash_engine(commands, auth=False, wp_pass=None):
+  q = 'mysql'
+  if auth:
+    q = q + (" -uroot --password='%s'" % (wp_pass))
+  for command in commands:
+    os.system('%s -e "%s"' % (q, command))
+
+# Networking
+
+def get_info():
+  hname = socket.gethostname()
+  hfile = '/etc/hosts'
+  ip = get_ip('eth0')
+  return (hfile, hname, ip)
+
+def populate(hfile, hname, ip):
+  with open(hfile, 'a') as f:
+    f.write('%s	%s' % (ip, hname))
+
+def fqdn_check():
+  info = get_info()
+  if os.stat(info[0]).st_size <= 158:
+    populate(info[0], info[1], info[2])
+
+def get_ip(ifname):
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  return socket.inet_ntoa(fcntl.ioctl(
+    s.fileno(),
+    0x8915, # SIOCGIFADDR
+    struct.pack('256s', ifname[:15])
+  )[20:24])
+
+# Main
+
+def selecter(choice):
+  func_dict = {'1': cPanel, '2': plesk, '3': LAMP, '4': wordpress}
+  return func_dict[choice]
+
+def main():
+  print("Please select an option to install.\n")
+  choice = raw_input("1 :: cPanel\n2 :: plesk\n3 :: LAMP Stack\n4 :: Wordpress\n\nChoice: ")
+  func = selecter(choice)
+  fqdn_check()
+  yum_engine(('wget',))
+  func()
+
+main()
